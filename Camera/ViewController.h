1580967461aa59a58c4a1f2febce9a98130204a2
Brian Olencki <bolencki13@gmail.com>
2015-05-23 18:05:09 -0400

    Initial Commit

---
+++ Camera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Camera
+//
+//  Created by Brian Olencki on 5/23/15.
+//  Copyright (c) 2015 bolencki13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
